258aac23ce9146886356bf335ec1bb1c1fb8fffc
Po <poweif@gmail.com>
2015-03-19 00:03:56 +0800

    Add katex example

---
+++ pub/katex/main.py
@@ -0,0 +1,23 @@
+from md import *
+
+kd("""
+
+## Title ##
+
+This is how you would write $c = \\frac{1}{2}$.
+I'll remember this ok?
+
+- $a=b$
+- $b=1$
+- $f(x) = x + 1$
+- $x^2$
+- $12345$
+- suzuka-san 
+""") 
+
+kd("""
+
+## Adachi from the hospital ##
+
+""") 
+
